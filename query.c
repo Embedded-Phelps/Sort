13e5ddba95e195032dc3b99527a0c9a242016787
Embedded-Phelps <phelpskwok@hotmail.com>
2017-11-22 14:18:40 -0700

    Handled the situation where the number of testcases is too large for the RAM; clean and commented the code

--- query.c
+++ query.c
@@ -5,9 +5,10 @@
  *              testcases from a specified binary input file. If there is a tie for the
  *              closest, return the lower one.
  * author:      Shuting Guo
- * Date:
+ * Date:        Nov. 22nd 2017
  ***************************************************************************************/
 
+/*********************************** Header ***********************************/
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -20,14 +21,14 @@
 #define RUN_SIZE        (8192)
 #define BLOCK_SIZE      (64)
 #else
-#define TOTAL_DATA_NUM  (1048576)
-#define NUM_WAYS        (512)
-#define RUN_SIZE        (2048)
-#define BLOCK_SIZE      (4)
+#define TOTAL_DATA_NUM  (8)
+#define NUM_WAYS        (2)
+#define RUN_SIZE        (4)
+#define BLOCK_SIZE      (2)
 #endif
 
 #define UNINT_MAX       (0xFFFFFFFF)
-
+#define NUM_TESTCASES_THRESHOLD (8000)
 
 /*********************************** Structures ***********************************/
 
@@ -77,47 +78,116 @@ void externalSort(char * input_file, char * output_file, unsigned int num_ways, 
  */
 unsigned int findMatch(char * data_file, unsigned int testcase);
 
+/* Utility functions */
 
+// swap two bytes
 void swapByte(unsigned char * xp, unsigned char * yp);
+// Swap two Integers
 void swapInt(unsigned int * xp, unsigned int * yp);
+// Convert the endianness of the first 'num' numbers of integers in 'data_buf'
 void convertEndianness(unsigned int * data_buf, unsigned int num);
+// Sort the elements in the 'arr' in ascending order with the quickSort algorithm
 void quickSort(unsigned int *arr, int low, int high);
+// Parition function used by the quicksort function
 int partition(unsigned int *arr, int low, int high);
-
+// Get the index of the left child of a min heap node of index 'nodeZ_index'
 unsigned int minHeap_getLeft(unsigned int node_index);
+// Get the index of the right child of a min heap node of index 'nodeZ_index'
 unsigned int MinHeap_getRight(unsigned int node_index);
+// Get the minimum node in a min heap structure
 MinHeapNode_t MinHeap_getMin(MinHeap_t * harr);
+// Heapify a heap into a min heap organization
 void MinHeap_heapify(MinHeap_t *harr, unsigned node_index);
+// Swap two min heap nodes
 void swapMinHeapNode(MinHeapNode_t * x, MinHeapNode_t * y);
+// Replace the minimum min heap node with a new min heap node x
 void MinHeap_replaceMin(MinHeap_t * harr, MinHeapNode_t x);
+// Construct a min heap structure with 'size' numbers of min heap nodes pointed to by 'a'
 MinHeap_t MinHeap_constructHeap(MinHeapNode_t * a, unsigned int size);
 
+/*********************************** Main ***********************************/
 
 int main(int argc, char ** argv)
 {
     char sorted_file[] = "sorted.bin";
     unsigned int num_testcase, i;
     unsigned int * user_testcase;
+    FILE *ptemp_file;
+    unsigned int temp;
+    size_t result;
     
+    /* Check command line input */
     if(argc == 1) {printf("ERROR: no input file.\n"); exit(-1);}
-
-    /* Read user-input testcases */
-    scanf("%u", &num_testcase);
-    user_testcase = (unsigned int *)malloc(sizeof(unsigned int)*num_testcase);
-    if(!user_testcase) {printf("ERROR: testcases buffer not suscessfully allocated.\n"); exit(-1);}
-    for(i=0; i<num_testcase; i++)
-    {
-        scanf("%u", user_testcase+i);
-    }
-
+    
     /* Sorting the input data file in ascending order and write to the 'sorted_file' */
     externalSort(*(argv+1), sorted_file, NUM_WAYS, RUN_SIZE);
     
-    /* Find the matching number for each testcase and print it out*/
-    for(i=0; i<num_testcase; i++)
+    /* Read user-input testcases */
+    scanf("%u", &num_testcase);
+    
+    /* When the number of testcases is too large for the RAM */
+    /* The first 1/5's matches are found and stored in a file first */
+    /* The program then takes in the rest of the testcases */
+    /* Print out the matched number from the file first*/
+    /* And do the match for the rest of the testcases and print out the results */
+    if (num_testcase > NUM_TESTCASES_THRESHOLD)
     {
-        *(user_testcase+i) = findMatch(sorted_file, *(user_testcase+i));
-        printf("%u\n", *(user_testcase+i));
+        user_testcase = (unsigned int *)malloc(sizeof(unsigned int)*(num_testcase/5));
+        if(!user_testcase) {printf("ERROR: testcases buffer not suscessfully allocated.\n"); exit(-1);}
+        for(i=0; i<num_testcase/5; i++)
+        {
+            scanf("%u", user_testcase+i);
+        }
+        ptemp_file = fopen("temp_out.bin", "wb");
+        if (!ptemp_file) {fputs ("File error", stderr); exit(1);}
+        
+        for(i=0; i<num_testcase/5; i++)
+        {
+            *(user_testcase+i) = findMatch(sorted_file, *(user_testcase+i));
+        }
+        fwrite(user_testcase, sizeof(unsigned int), num_testcase/5, ptemp_file);
+        fclose(ptemp_file);
+        free(user_testcase);
+        user_testcase = (unsigned int *)malloc(sizeof(unsigned int)*(num_testcase-num_testcase/5));
+        if(!user_testcase) {printf("ERROR: testcases buffer not suscessfully allocated.\n"); exit(-1);}
+        for(i=0; i<(num_testcase-num_testcase/5); i++)
+        {
+            scanf("%u", user_testcase+i);
+        }
+        ptemp_file = fopen("temp_out.bin", "rb");
+        if (!ptemp_file) {fputs ("File error", stderr); exit(1);}
+        for(i=0; i<num_testcase/5; i++)
+        {
+            fseek(ptemp_file, i*sizeof(unsigned int), SEEK_SET);
+            result = fread(&temp, sizeof(unsigned int), 1, ptemp_file);
+            if (result != 1) {fputs("Reading error", stderr); exit(3);}
+            printf("%u\n", temp);
+        }
+        fclose(ptemp_file);
+        for(i=0; i<(num_testcase-num_testcase/5); i++)
+        {
+            *(user_testcase+i) = findMatch(sorted_file, *(user_testcase+i));
+            printf("%u\n", *(user_testcase+i));
+        }
+    }
+    
+    /* When the number of testcases is not too large for the RAM */
+    /* Directly do the match for all of the testcases and print out the results */
+    else
+    {
+        user_testcase = (unsigned int *)malloc(sizeof(unsigned int)*num_testcase);
+        if(!user_testcase) {printf("ERROR: testcases buffer not suscessfully allocated.\n"); exit(-1);}
+        for(i=0; i<num_testcase; i++)
+        {
+            scanf("%u", user_testcase+i);
+        }
+
+        /* Find the matching number for each testcase and print it out*/
+        for(i=0; i<num_testcase; i++)
+        {
+            *(user_testcase+i) = findMatch(sorted_file, *(user_testcase+i));
+            printf("%u\n", *(user_testcase+i));
+        }
     }
     /* Free the buffer */
     free(user_testcase);
@@ -125,6 +195,22 @@ int main(int argc, char ** argv)
     return 0;
 }
 
+/*********************************** Function ***********************************/
+
+/* @Function Name: externalSort
+ * @Inputs:
+ *  - input_file: name of the input data file to sort
+ *  - output_file: name of the output file
+ *  - num_ways: number of runs to be merged
+ *  - run_size: size of a run (can fit in RAM)
+ * @Description:
+ *  sort a binary data file in ascending order
+ *  1) read input_file such that at most 'run_size' element are read at a time
+ *     Do the following for each of the 'num_ways' runs:
+ *         a) Sort the run using 'QuickSort' algorithm
+ *         b) Store the sorted run in a temperary file on disk
+ *  2) Merge the sorted files using 'K-way Merge' algorithm
+ */
 void externalSort(char * input_file, char * output_file, unsigned int num_ways, unsigned int run_size)
 {
     FILE * pdata_file, * ptemp_file;
@@ -237,6 +323,16 @@ void externalSort(char * input_file, char * output_file, unsigned int num_ways, 
     result = fclose(pdata_file);
 }
 
+/* @Function Name: findMatch
+ * @Inputs:
+ *  - data_file: name of the data file to search from
+ *  - testcase: the number to find match
+ * @Return:
+ *  - matched number
+ * @Description:
+ *  find the matched or cloest number to 'testcase' from the 'data_file'
+ *  with 'binary search' algorithm
+ */
 unsigned int findMatch(char * data_file, unsigned int testcase)
 {
     FILE * pdata_file;
@@ -244,16 +340,17 @@ unsigned int findMatch(char * data_file, unsigned int testcase)
     int delta, delta_min;
     unsigned int temp[3];
     
-    // Open the sorted data file
+    /* Open the sorted data file */
     pdata_file = fopen(data_file, "rb");
     if (!pdata_file) {fputs ("File error", stderr); exit(1);}
     
+    /* Perfoming binary search to find a match */
     while(rb >= lb)
     {
         mid = lb + (rb - lb)/2;
         fseek(pdata_file, mid*sizeof(unsigned int), SEEK_SET);
         fread(temp, sizeof(unsigned int), 1, pdata_file);
-        if(temp[0] == testcase)
+        if(temp[0] == testcase) // An exact match is found
         {
             fclose(pdata_file);
             return temp[0];
@@ -268,6 +365,9 @@ unsigned int findMatch(char * data_file, unsigned int testcase)
         }
     }
     
+    /* If an exact match is not found */
+    /* Obtain the two neighbor numbers of the number at position 'mid' */
+    /* Taking into account the BOF and EOF situations */
     if(mid == 0)
     {
         fseek(pdata_file, mid * sizeof(unsigned int), SEEK_SET);
@@ -286,6 +386,8 @@ unsigned int findMatch(char * data_file, unsigned int testcase)
         fread(temp, sizeof(unsigned int), 3, pdata_file);
         mid = 3;
     }
+    
+    /* Find the smallest number that is closest to the target number */
     lb = 0;
     delta_min = abs(int(temp[0]-testcase));
     for(rb=1; rb<mid; rb++)
@@ -297,10 +399,13 @@ unsigned int findMatch(char * data_file, unsigned int testcase)
             lb = rb;
         }
     }
+    
+    /* Close the data file and return the number found */
     fclose(pdata_file);
     return temp[lb];
 }
 
+// Convert the endianness of the first 'num' numbers of integers in 'data_buf'
 void convertEndianness(unsigned int * data_buf, unsigned int num)
 {
     unsigned int i;
@@ -315,6 +420,7 @@ void convertEndianness(unsigned int * data_buf, unsigned int num)
     }
 }
 
+// Sort the elements in the 'arr' in ascending order with the quickSort algorithm
 void quickSort(unsigned int *arr, int low, int high)
 {
     int pi;
@@ -326,6 +432,7 @@ void quickSort(unsigned int *arr, int low, int high)
     }
 }
 
+// Parition function used by the quicksort function
 int partition(unsigned int *arr, int low, int high)
 {
     int pivot = *(arr+high);
@@ -342,6 +449,7 @@ int partition(unsigned int *arr, int low, int high)
     return (i+1);
 }
 
+// swap two bytes
 void swapByte(unsigned char * xp, unsigned char * yp)
 {
     if(xp == yp)
@@ -351,6 +459,7 @@ void swapByte(unsigned char * xp, unsigned char * yp)
     *xp = *xp ^ *yp;
 }
 
+// Swap two Integers
 void swapInt(unsigned int * xp, unsigned int * yp)
 {
     if(xp == yp)
@@ -360,22 +469,25 @@ void swapInt(unsigned int * xp, unsigned int * yp)
     *xp = *xp ^ *yp;
 }
 
-/***********************/
+// Get the index of the left child of a min heap node of index 'nodeZ_index'
 unsigned int minHeap_getLeft(unsigned int node_index)
 {
     return (2*node_index+1);
 }
 
+// Get the index of the right child of a min heap node of index 'nodeZ_index'
 unsigned int MinHeap_getRight(unsigned int node_index)
 {
     return (2*node_index+2);
 }
 
+// Get the minimum node in a min heap structure
 MinHeapNode_t MinHeap_getMin(MinHeap_t * mp)
 {
     return mp->harr_n[0];
 }
 
+// Heapify a heap into a min heap organization
 void MinHeap_heapify(MinHeap_t * mp, unsigned node_index)
 {
     unsigned int left = minHeap_getLeft(node_index);
@@ -392,6 +504,7 @@ void MinHeap_heapify(MinHeap_t * mp, unsigned node_index)
     }
 }
 
+// Swap two min heap nodes
 void swapMinHeapNode(MinHeapNode_t * x, MinHeapNode_t * y)
 {
     MinHeapNode_t temp = *x;
@@ -399,12 +512,14 @@ void swapMinHeapNode(MinHeapNode_t * x, MinHeapNode_t * y)
     *y = temp;
 }
 
+// Replace the minimum min heap node with a new min heap node x
 void MinHeap_replaceMin(MinHeap_t * mp, MinHeapNode_t x)
 {
     mp->harr_n[0] = x;
     MinHeap_heapify(mp, 0);
 }
 
+// Construct a min heap structure with 'size' numbers of min heap nodes pointed to by 'a'
 MinHeap_t MinHeap_constructHeap(MinHeapNode_t * a, unsigned int size)
 {
     MinHeap_t mp;
